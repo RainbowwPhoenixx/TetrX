947d9ea7c78b2dafd6242af275cb88fab57ff378
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2019-11-18 22:46:37 +0100

    Created header file to include all header file of the tetris engine for easier inclusion from other parts of the program.

---
+++ tetris_engine/tetris_engine.h
@@ -0,0 +1,10 @@
+#ifndef __TETRIS_ENGINE_H__
+#define __TETRIS_ENGINE_H__
+
+// This header file is a wrapper for all of the other header files of the tetris engine
+#include "general_library.h"
+#include "type_shape.h"
+#include "type_mino.h"
+#include "type_matrix.h"
+
+#endif
