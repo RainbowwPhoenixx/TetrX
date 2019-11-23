d1df561cd1cc27b2f57c31505d2f5ab35c4d4f91
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2019-11-23 22:24:36 +0100

    Corrected bug due to erroneous typecasting.

--- tetris_engine/type_tetrimino.c
+++ tetris_engine/type_tetrimino.c
@@ -3,43 +3,43 @@
 #define TETRIMINO_SPAWN_X 4
 #define TETRIMINO_SPAWN_Y 20
 
-static const Tcoordinate_diff L_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
+static Tcoordinate_diff L_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
   {{-1,  0}, { 0,  0}, { 1,  0}, { 1,  1}}, // Initial pos for the L
   {{ 0, -1}, { 1, -1}, { 0,  0}, { 0,  1}},
   {{-1,  0}, { 0,  0}, { 1,  0}, {-1, -1}},
   {{ 0, -1}, { 0,  0}, {-1,  1}, { 0,  1}},
 };
-static const Tcoordinate_diff J_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
+static Tcoordinate_diff J_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
   {{-1,  0}, { 0,  0}, { 1,  0}, {-1,  1}}, // Initial pos for the J
   {{ 0, -1}, { 0,  0}, { 0,  1}, { 1,  1}},
   {{-1,  0}, { 0,  0}, { 1,  0}, { 1, -1}},
   {{ 0, -1}, { 0,  0}, { 0,  1}, {-1, -1}},
 };
-static const Tcoordinate_diff S_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
+static Tcoordinate_diff S_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
   {{-1,  0}, { 0,  0}, { 0,  1}, { 1,  1}}, // Initial pos for the S
   {{ 0,  1}, { 0,  0}, { 1,  0}, { 1, -1}},
   {{ 1,  0}, { 0,  0}, { 0, -1}, {-1, -1}},
   {{ 0, -1}, { 0,  0}, {-1,  0}, {-1,  1}},
 };
-static const Tcoordinate_diff Z_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
+static Tcoordinate_diff Z_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
   {{ 0,  0}, { 1,  0}, { 0,  1}, {-1,  1}}, // Initial pos for the Z
   {{ 0, -1}, { 0,  0}, { 1,  0}, { 1,  1}},
   {{ 0,  0}, {-1,  0}, { 0, -1}, { 1, -1}},
   {{ 0,  1}, { 0,  0}, {-1,  0}, {-1, -1}},
 };
-static const Tcoordinate_diff O_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
+static Tcoordinate_diff O_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
   {{ 0,  0}, { 1,  0}, { 0,  1}, { 1,  1}}, // Initial pos for the O
   {{ 0,  0}, { 1,  0}, { 0,  1}, { 1,  1}},
   {{ 0,  0}, { 1,  0}, { 0,  1}, { 1,  1}},
   {{ 0,  0}, { 1,  0}, { 0,  1}, { 1,  1}},
 };
-static const Tcoordinate_diff I_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
+static Tcoordinate_diff I_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
   {{-1,  0}, { 0,  0}, { 1,  0}, { 2,  0}}, // Initial pos for the I
   {{ 1, -1}, { 1,  0}, { 1,  1}, { 1,  2}},
   {{-1, -1}, { 0, -1}, { 1, -1}, { 2, -1}},
   {{ 0, -1}, { 0,  0}, { 0,  1}, { 0,  2}},
 };
-static const Tcoordinate_diff T_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
+static Tcoordinate_diff T_initial_mino_positions[4][4][2] = { // [rotation_state][mino_index][x or y]
   {{-1,  0}, { 0,  0}, { 1,  0}, { 0,  1}}, // Initial pos for the T
   {{ 0, -1}, { 0,  0}, { 0,  1}, { 1,  0}},
   {{-1,  0}, { 0,  0}, { 1,  0}, { 0, -1}},
@@ -90,16 +90,16 @@ Ttetrimino createTetrimino (Tshape tetrimino_shape) {
   setTetriminoX (&t, TETRIMINO_SPAWN_X);
   setTetriminoY (&t, TETRIMINO_SPAWN_Y);
 
-  Tcoordinate_diff **initial_mino_pos;
+  Tcoordinate_diff (*initial_mino_pos)[2];
 
   switch (tetrimino_shape) {
-    case L: initial_mino_pos = (Tcoordinate_diff **) L_initial_mino_positions[0]; break;
-    case J: initial_mino_pos = (Tcoordinate_diff **) J_initial_mino_positions[0]; break;
-    case S: initial_mino_pos = (Tcoordinate_diff **) S_initial_mino_positions[0]; break;
-    case Z: initial_mino_pos = (Tcoordinate_diff **) Z_initial_mino_positions[0]; break;
-    case O: initial_mino_pos = (Tcoordinate_diff **) O_initial_mino_positions[0]; break;
-    case I: initial_mino_pos = (Tcoordinate_diff **) I_initial_mino_positions[0]; break;
-    case T: initial_mino_pos = (Tcoordinate_diff **) T_initial_mino_positions[0]; break;
+    case L: initial_mino_pos = L_initial_mino_positions[0]; break;
+    case J: initial_mino_pos = J_initial_mino_positions[0]; break;
+    case S: initial_mino_pos = S_initial_mino_positions[0]; break;
+    case Z: initial_mino_pos = Z_initial_mino_positions[0]; break;
+    case O: initial_mino_pos = O_initial_mino_positions[0]; break;
+    case I: initial_mino_pos = I_initial_mino_positions[0]; break;
+    case T: initial_mino_pos = T_initial_mino_positions[0]; break;
     case EMPTY: break;
   }
 
