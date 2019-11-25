6abc57543749222a3c542e9c9b4eb8f050991aa6
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2019-11-25 23:02:56 +0100

    Added movement header to global header.

--- tetris_engine/tetris_engine.h
+++ tetris_engine/tetris_engine.h
@@ -4,6 +4,7 @@
 // This header file is a wrapper for all of the other header files of the tetris engine
 #include "general_library.h"
 #include "type_shape.h"
+#include "type_movement.h"
 #include "type_mino.h"
 #include "type_tetrimino.h"
 #include "type_matrix.h"
