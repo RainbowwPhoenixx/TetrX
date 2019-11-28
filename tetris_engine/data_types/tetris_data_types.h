38c522c5286faea0475c1be43ea93b69dc8f14fa
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2019-11-28 14:59:31 +0100

    Changed program structure with two subfolders in tetris_engine.

---
+++ tetris_engine/data_types/tetris_data_types.h
@@ -0,0 +1,14 @@
+#ifndef __TETRIS_DATA_TYPES_H__
+#define __TETRIS_DATA_TYPES_H__
+
+// This header file is a wrapper for all of the other header files of the tetris engine
+#include "general_library.h"
+#include "type_shape.h"
+#include "type_movement.h"
+#include "type_mino.h"
+#include "type_tetrimino.h"
+#include "type_matrix.h"
+#include "type_next_queue.h"
+#include "type_board.h"
+
+#endif
--- tetris_engine/tetris_engine.h
+++ tetris_engine/tetris_engine.h
@@ -1,14 +1,6 @@
 #ifndef __TETRIS_ENGINE_H__
 #define __TETRIS_ENGINE_H__
 
-// This header file is a wrapper for all of the other header files of the tetris engine
-#include "general_library.h"
-#include "type_shape.h"
-#include "type_movement.h"
-#include "type_mino.h"
-#include "type_tetrimino.h"
-#include "type_matrix.h"
-#include "type_next_queue.h"
-#include "type_board.h"
+#include "../tetris_data_types.h"
 
 #endif
