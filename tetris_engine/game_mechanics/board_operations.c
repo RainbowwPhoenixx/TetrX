c378e3638bf69442951a8257f33fdaf755013927
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2019-12-02 21:12:14 +0100

    Mechanics update : - Added tetrimino locking - Added capacity to get a new tetrimino from the queue - Implemented SRS

--- tetris_engine/game_mechanics/board_operations.c
+++ tetris_engine/game_mechanics/board_operations.c
@@ -1,5 +1,7 @@
 #include "board_operations.h"
 
+#define MINIMUM_QUEUE_LENGTH 8
+
 static Tcoordinate_diff not_I_kicks[4][5][2] = {
   {{ 0,  0}, { -1, 0}, {-1,  1}, { 0, -2}, {-1, -2}},
   {{ 0,  0}, { 1,  0}, { 1, -1}, { 0,  2}, { 1,  2}},
@@ -48,6 +50,27 @@ bool isBoardStateValid (Tboard *b) {
 
 // Game mechanics implementation
 
+static void lockActiveTetrimino (Tboard *b) {
+  Ttetrimino *t = getBoardActiveTetrimino (b);
+  Tmatrix *m = getBoardMatrix (b);
+
+  for (Tbyte i = 0; i < NUMBER_OF_MINOS; i++) {
+    Tmino *tmp_mino = getIthMino (t, i);
+    setMatrixShapeAtPos (m, getTetriminoX (t) + getMinoXDiff (tmp_mino), getTetriminoY (t) + getMinoYDiff (tmp_mino), getTetriminoShape (t));
+  }
+}
+static void popTetriminoFromQueue (Tboard *b) {
+  Ttetrimino *t = getBoardActiveTetrimino (b);
+  Tnext_queue *next_queue = getBoardNextQueue (b);
+  *t = createTetrimino (getIthNextPiece (next_queue, 0));
+
+  advanceNextQueue (next_queue);
+
+  if (getNextQueueLength (next_queue) < MINIMUM_QUEUE_LENGTH) {
+    addBagToNextQueue (next_queue);
+  }
+}
+
 static void performMoveLeft (Tboard *b) {
   Tboard tmp_board;
   copyBoard (&tmp_board, b);
@@ -90,27 +113,75 @@ static void performHardDrop  (Tboard *b) {
     moveTetriminoDown (getBoardActiveTetrimino (&tmp_board));
   } while(isBoardStateValid (&tmp_board));
 
+  lockActiveTetrimino (b);
+  popTetriminoFromQueue (b);
 }
 static void performRotateCW (Tboard *b) {
-  Tboard tmp_board;
-  copyBoard (&tmp_board, b);
+  Ttetrimino *t = getBoardActiveTetrimino (b);
 
-  moveTetriminoCW (getBoardActiveTetrimino (b));
+  if (getTetriminoShape (t) != O) { // The O doesn't rotate
+    Tboard tmp_board;
+    Tcoordinate_diff (*kicks)[2];
+    copyBoard (&tmp_board, b);
 
-  // If the new state is not valid, revert to previous state
-  if (!isBoardStateValid (b)) {
-    copyBoard (b, &tmp_board);
+    if (getTetriminoShape (t) == I) {
+      kicks = I_kicks[getTetriminoRotationState (t)];
+    } else {
+      kicks = not_I_kicks[getTetriminoRotationState (t)];
+    }
+
+    // Rotate the piece
+    moveTetriminoCW (t);
+    Ttetrimino piece_no_kicks = *t;
+
+    // Attempt to kick the piece
+    int i = 0;
+    do {
+      *t = piece_no_kicks;
+      setTetriminoX (t, getTetriminoX (t) + kicks[i][0]);
+      setTetriminoY (t, getTetriminoY (t) + kicks[i][1]);
+
+      i++;
+    } while(!isBoardStateValid (b) && i < 5);
+
+    // If the new state is not valid, revert to previous state
+    if (!isBoardStateValid (b)) {
+      copyBoard (b, &tmp_board);
+    }
   }
 }
 static void performRotateCCW (Tboard *b) {
-  Tboard tmp_board;
-  copyBoard (&tmp_board, b);
+  Ttetrimino *t = getBoardActiveTetrimino (b);
 
-  moveTetriminoCCW (getBoardActiveTetrimino (b));
+  if (getTetriminoShape (t) != O) { // The O doesn't rotate
+    Tboard tmp_board;
+    Tcoordinate_diff (*kicks)[2];
+    copyBoard (&tmp_board, b);
+    
+    // Rotate the piece
+    moveTetriminoCCW (t);
+    Ttetrimino piece_no_kicks = *t;
 
-  // If the new state is not valid, revert to previous state
-  if (!isBoardStateValid (b)) {
-    copyBoard (b, &tmp_board);
+    if (getTetriminoShape (t) == I) {
+      kicks = I_kicks[getTetriminoRotationState (t)];
+    } else {
+      kicks = not_I_kicks[getTetriminoRotationState (t)];
+    }
+
+    // Attempt to kick the piece
+    int i = 0;
+    do {
+      *t = piece_no_kicks;
+      setTetriminoX (t, getTetriminoX (t) - kicks[i][0]);
+      setTetriminoY (t, getTetriminoY (t) - kicks[i][1]);
+
+      i++;
+    } while(!isBoardStateValid (b) && i < 5);
+
+    // If the new state is not valid, revert to previous state
+    if (!isBoardStateValid (b)) {
+      copyBoard (b, &tmp_board);
+    }
   }
 }
 static void performHold (Tboard *b) {
