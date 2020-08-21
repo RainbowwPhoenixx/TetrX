c21b787426cd7b4cc7457771d72a8c5ab7e770fe
Antoine/RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2020-08-21 14:04:29 +0200

    Got rid of redundant memory, resulting in a small performance boost. Stopped storing the mino data into the tetrimino struct, instead storing a single pointer to an static array. This makes copying the tetrimino struct a much less expensive operation.

--- tetris_engine/data_types/type_tetrimino.h
+++ tetris_engine/data_types/type_tetrimino.h
@@ -11,7 +11,7 @@ typedef enum {R0=0, R90=1, R180=2, R270=3} Trotation_state;
 
 typedef struct {
   Tshape shape;                   // Shape of the tetrimino
-  Tmino minos[NUMBER_OF_MINOS];   // Components of the tetrimino
+  Tmino *minos;   // Components of the tetrimino
   Tcoordinate x, y;               // Position of the tetrimino on the matrix
   Trotation_state rotation_state; // Stores the current rotation state
 } Ttetrimino;
@@ -21,7 +21,9 @@ Tshape getTetriminoShape (Ttetrimino *t);
 void setTetriminoShape (Ttetrimino *t, Tshape new_shape);
 
 Tmino *getIthMino (Ttetrimino *t, int i);
-void setIthMino (Ttetrimino *t, int i, Tmino new_mino);
+// You can only set the minos as a predefined list defined at the
+// top of type_tetrimino.c
+void setMinos (Ttetrimino *t, Tmino *minos);
 
 Tcoordinate getTetriminoX (Ttetrimino *t);
 void setTetriminoX (Ttetrimino *t, Tcoordinate new_x);
